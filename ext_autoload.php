bc57887ccfb90104db30e498ccb98faf6b5dfd43
Felix Nagel <info@felixnagel.com>
2014-08-23 15:59:20 +0200

    force CGL

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'tx_deletefiles_cleaner' => t3lib_extMgm::extPath('deletefiles', 'tasks/class.tx_deletefiles_cleaner.php'),
-    'tx_deletefiles_cleaner_addfields' => t3lib_extMgm::extPath('deletefiles', 'tasks/class.tx_deletefiles_cleaner_addFields.php'),
+	'tx_deletefiles_cleaner' => t3lib_extMgm::extPath('deletefiles', 'tasks/class.tx_deletefiles_cleaner.php'),
+	'tx_deletefiles_cleaner_addfields' => t3lib_extMgm::extPath('deletefiles', 'tasks/class.tx_deletefiles_cleaner_addFields.php'),
 );
 ?>
--- tasks/class.tx_deletefiles_cleaner_addFields.php
+++ tasks/class.tx_deletefiles_cleaner_addFields.php
@@ -1,8 +1,9 @@
 <?php
+
 class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldProvider {
 
-	public function getAdditionalFields(array &$taskInfo, $task, tx_scheduler_Module $schedulerModule) { 
-	
+	public function getAdditionalFields(array &$taskInfo, $task, tx_scheduler_Module $schedulerModule) {
+
 		// process fields
 		if (empty($taskInfo['deletefiles_directory'])) {
 			if ($schedulerModule->CMD == 'add') {
@@ -31,65 +32,64 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 				$taskInfo['deletefiles_method'] = 0;
 			}
 		}
-		
+
 		// render directory field
 		$fieldID = 'task_deletefiles_directory';
-		
+
 		$fieldCode = '<input type="text" name="tx_scheduler[deletefiles_directory]" id="' . $fieldID . '" value="' . htmlspecialchars($taskInfo['deletefiles_directory']) . '">';
-		
+
 		$label = $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_directory');
 		$label = t3lib_BEfunc::wrapInHelp('deletefiles', $fieldID, $label);
 		$additionalFields[$fieldID] = array(
 			'code' => $fieldCode,
 			'label' => $label,
-			'cshKey'   => '_MOD_tools_txschedulerM1',
+			'cshKey' => '_MOD_tools_txschedulerM1',
 			'cshLabel' => $fieldID
 		);
-		
 
-		
+
 		// render time field
 		$fieldID = 'task_deletefiles_time';
 		$fieldValueArray = $this->getTimes();
-		
+
 		$fieldCode = '<select name="tx_scheduler[deletefiles_time]" id="' . $fieldID . '">';
 		foreach ($fieldValueArray as $deletefiles_time => $label) {
-			$fieldCode .= "\t" . '<option value="' .  htmlspecialchars($deletefiles_time) . '"' . (($deletefiles_time ==$taskInfo['deletefiles_time']) ? ' selected="selected"' : '') . '>' . $label . '</option>';
+			$fieldCode .= "\t" . '<option value="' . htmlspecialchars($deletefiles_time) . '"' . (($deletefiles_time == $taskInfo['deletefiles_time']) ? ' selected="selected"' : '') . '>' . $label . '</option>';
 		}
 		$fieldCode .= '</select>';
-		
+
 		$label = $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_time');
 		$label = t3lib_BEfunc::wrapInHelp('deletefiles', $fieldID, $label);
 		$additionalFields[$fieldID] = array(
 			'code' => $fieldCode,
 			'label' => $label,
-			'cshKey'   => '_MOD_tools_txschedulerM1',
+			'cshKey' => '_MOD_tools_txschedulerM1',
 			'cshLabel' => $fieldID
 		);
-		
-		
+
+
 		// render method field
 		$fieldID = 'task_deletefiles_method';
 		$fieldValueArray = $this->getMethods();
-		
+
 		$fieldCode = '<select name="tx_scheduler[deletefiles_method]" id="' . $fieldID . '">';
 		foreach ($fieldValueArray as $deletefiles_method => $label) {
-			$fieldCode .= "\t" . '<option value="' .  htmlspecialchars($deletefiles_method) . '"' . (($deletefiles_method ==$taskInfo['deletefiles_method']) ? ' selected="selected"' : '') . '>' . $label . '</option>';
+			$fieldCode .= "\t" . '<option value="' . htmlspecialchars($deletefiles_method) . '"' . (($deletefiles_method == $taskInfo['deletefiles_method']) ? ' selected="selected"' : '') . '>' . $label . '</option>';
 		}
 		$fieldCode .= '</select>';
-		
+
 		$label = $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_method');
 		$label = t3lib_BEfunc::wrapInHelp('deletefiles', $fieldID, $label);
 		$additionalFields[$fieldID] = array(
 			'code' => $fieldCode,
 			'label' => $label,
-			'cshKey'   => '_MOD_tools_txschedulerM1',
+			'cshKey' => '_MOD_tools_txschedulerM1',
 			'cshLabel' => $fieldID
 		);
 
-        return $additionalFields;
-    }
-	
+		return $additionalFields;
+	}
+
 	public function getTimes() {
 		// TODO add a hook for adding times
 		return array(
@@ -123,22 +123,22 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 	/**
 	 * Validates the additional fields' values
 	 *
-	 * @param	array	$submittedData An array containing the data submitted by the add/edit task form
-	 * @param	tx_scheduler_Module	$schedulerModule Reference to the scheduler backend module
-	 * @return	boolean	True if validation was ok (or selected class is not relevant), false otherwise
+	 * @param    array $submittedData An array containing the data submitted by the add/edit task form
+	 * @param    tx_scheduler_Module $schedulerModule Reference to the scheduler backend module
+	 * @return    boolean    True if validation was ok (or selected class is not relevant), false otherwise
 	 */
 	public function validateAdditionalFields(array &$submittedData, tx_scheduler_Module $schedulerModule) {
 		$validInput = TRUE;
 
 		// clean data
-        $submittedData['deletefiles_directory'] = 	trim($submittedData['deletefiles_directory'], "/");
-        $submittedData['deletefiles_time'] = 		trim($submittedData['deletefiles_time']);
-        $submittedData['deletefiles_method'] = 	$submittedData['deletefiles_method'];
-					
-		$path = $submittedData['deletefiles_directory'];		
-		if ( !(strlen($path) > 0 && file_exists(PATH_site . $path) && t3lib_div::isAllowedAbsPath(PATH_site . $path) && t3lib_div::validPathStr($path)) ) {
+		$submittedData['deletefiles_directory'] = trim($submittedData['deletefiles_directory'], "/");
+		$submittedData['deletefiles_time'] = trim($submittedData['deletefiles_time']);
+		$submittedData['deletefiles_method'] = $submittedData['deletefiles_method'];
+
+		$path = $submittedData['deletefiles_directory'];
+		if (!(strlen($path) > 0 && file_exists(PATH_site . $path) && t3lib_div::isAllowedAbsPath(PATH_site . $path) && t3lib_div::validPathStr($path))) {
 			$schedulerModule->addMessage(
-				sprintf( $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_notice_path_invalid'), $path ),
+				sprintf($GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_notice_path_invalid'), $path),
 				t3lib_FlashMessage::ERROR
 			);
 			$validInput = FALSE;
@@ -146,16 +146,16 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 
 		return $validInput;
 	}
-	
 
-    public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task) {
-        $task->deletefiles_directory = 	$submittedData['deletefiles_directory'];
-        $task->deletefiles_time = 		$submittedData['deletefiles_time'];
-        $task->deletefiles_method = 	$submittedData['deletefiles_method'];
-    }
+
+	public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task) {
+		$task->deletefiles_directory = $submittedData['deletefiles_directory'];
+		$task->deletefiles_time = $submittedData['deletefiles_time'];
+		$task->deletefiles_method = $submittedData['deletefiles_method'];
+	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/deletefiles/tasks/class.tx_deletefiles_cleaner_addFields.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/deletefiles/tasks/class.tx_deletefiles_cleaner_addFields.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/deletefiles/tasks/class.tx_deletefiles_cleaner_addFields.php']);
 }
 ?>
