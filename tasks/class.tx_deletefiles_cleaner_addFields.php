07a38fdcd37dfa3a5efd0e560ee38281af697bcb
Felix Nagel <info@felixnagel.com>
2014-08-23 16:40:20 +0200

    CGL

--- tasks/class.tx_deletefiles_cleaner_addFields.php
+++ tasks/class.tx_deletefiles_cleaner_addFields.php
@@ -1,8 +1,33 @@
 <?php
 
+/**
+ * Class tx_deletefiles_cleaner_addFields
+ */
 class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldProvider {
 
+	/**
+	 * @var \TYPO3\CMS\Lang\LanguageService
+	 */
+	protected $languageService;
+
+	/**
+	 * Construct class
+	 */
+	public function __construct() {
+		$this->languageService = $GLOBALS['LANG'];
+	}
+
+	/**
+	 * Gets additional fields to render in the form to add/edit a task
+	 *
+	 * @param array $taskInfo Values of the fields from the add/edit task form
+	 * @param \TYPO3\CMS\Scheduler\Task\AbstractTask $task The task object being edited. Null when adding a task!
+	 * @param tx_scheduler_Module|\TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule Reference to the scheduler backend module
+	 *
+	 * @return array A two dimensional array, array('Identifier' => array('fieldId' => array('code' => '', 'label' => '', 'cshKey' => '', 'cshLabel' => ''))
+	 */
 	public function getAdditionalFields(array &$taskInfo, $task, tx_scheduler_Module $schedulerModule) {
+
 
 		// process fields
 		if (empty($taskInfo['deletefiles_directory'])) {
@@ -38,7 +63,7 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 
 		$fieldCode = '<input type="text" name="tx_scheduler[deletefiles_directory]" id="' . $fieldID . '" value="' . htmlspecialchars($taskInfo['deletefiles_directory']) . '">';
 
-		$label = $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_directory');
+		$label = $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_directory');
 		$label = t3lib_BEfunc::wrapInHelp('deletefiles', $fieldID, $label);
 		$additionalFields[$fieldID] = array(
 			'code' => $fieldCode,
@@ -58,7 +83,7 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 		}
 		$fieldCode .= '</select>';
 
-		$label = $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_time');
+		$label = $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_time');
 		$label = t3lib_BEfunc::wrapInHelp('deletefiles', $fieldID, $label);
 		$additionalFields[$fieldID] = array(
 			'code' => $fieldCode,
@@ -78,7 +103,7 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 		}
 		$fieldCode .= '</select>';
 
-		$label = $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_method');
+		$label = $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_label_method');
 		$label = t3lib_BEfunc::wrapInHelp('deletefiles', $fieldID, $label);
 		$additionalFields[$fieldID] = array(
 			'code' => $fieldCode,
@@ -90,42 +115,51 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 		return $additionalFields;
 	}
 
+	/**
+	 * @todo add a hook here
+	 *
+	 * @return array
+	 */
 	public function getTimes() {
-		// TODO add a hook for adding times
 		return array(
-			'0' => $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_all'),
-			'1h' => '1 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
-			'6h' => '6 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
-			'12h' => '12 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
-			'24h' => '24 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
-			'48h' => '48 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
-			'72h' => '72 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
-			'7d' => '7 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_d'),
-			'14d' => '14 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_d'),
-			'1m' => '1 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_m'),
-			'3m' => '3 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_m'),
-			'6m' => '6 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_m'),
-			'12m' => '12 ' . $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_m'),
+			'0' => $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_all'),
+			'1h' => '1 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
+			'6h' => '6 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
+			'12h' => '12 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
+			'24h' => '24 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
+			'48h' => '48 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
+			'72h' => '72 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_h'),
+			'7d' => '7 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_d'),
+			'14d' => '14 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_d'),
+			'1m' => '1 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_m'),
+			'3m' => '3 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_m'),
+			'6m' => '6 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_m'),
+			'12m' => '12 ' . $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_time_m'),
 		);
 	}
 
+	/**
+	 * @todo add a hook here
+	 *
+	 * @return array
+	 */
 	public function getMethods() {
-		// TODO add a hook for adding times
 		return array(
-			'delete_directory' => $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_directory'),
-			'delete_files' => $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_files'),
-			'delete_directories' => $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_directories'),
-			'delete_all' => $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_all'),
-			'delete_all_recursive' => $GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_all_recursive'),
+			'delete_directory' => $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_directory'),
+			'delete_files' => $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_files'),
+			'delete_directories' => $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_directories'),
+			'delete_all' => $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_all'),
+			'delete_all_recursive' => $this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_method_delete_all_recursive'),
 		);
 	}
 
 	/**
 	 * Validates the additional fields' values
 	 *
-	 * @param    array $submittedData An array containing the data submitted by the add/edit task form
-	 * @param    tx_scheduler_Module $schedulerModule Reference to the scheduler backend module
-	 * @return    boolean    True if validation was ok (or selected class is not relevant), false otherwise
+	 * @param array $submittedData An array containing the data submitted by the add/edit task form
+	 * @param tx_scheduler_Module|\TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule Reference to the scheduler backend module
+	 *
+	 * @return boolean TRUE if validation was ok (or selected class is not relevant), FALSE otherwise
 	 */
 	public function validateAdditionalFields(array &$submittedData, tx_scheduler_Module $schedulerModule) {
 		$validInput = TRUE;
@@ -133,12 +167,11 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 		// clean data
 		$submittedData['deletefiles_directory'] = trim($submittedData['deletefiles_directory'], "/");
 		$submittedData['deletefiles_time'] = trim($submittedData['deletefiles_time']);
-		$submittedData['deletefiles_method'] = $submittedData['deletefiles_method'];
 
 		$path = $submittedData['deletefiles_directory'];
 		if (!(strlen($path) > 0 && file_exists(PATH_site . $path) && t3lib_div::isAllowedAbsPath(PATH_site . $path) && t3lib_div::validPathStr($path))) {
 			$schedulerModule->addMessage(
-				sprintf($GLOBALS['LANG']->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_notice_path_invalid'), $path),
+				sprintf($this->languageService->sL('LLL:EXT:deletefiles/lang/locallang.xml:addfields_notice_path_invalid'), $path),
 				t3lib_FlashMessage::ERROR
 			);
 			$validInput = FALSE;
@@ -147,7 +180,14 @@ class tx_deletefiles_cleaner_addFields implements tx_scheduler_AdditionalFieldPr
 		return $validInput;
 	}
 
-
+	/**
+	 * Takes care of saving the additional fields' values in the task's object
+	 *
+	 * @param array $submittedData An array containing the data submitted by the add/edit task form
+	 * @param tx_scheduler_Task|\TYPO3\CMS\Scheduler\Task\AbstractTask $task Reference to the scheduler backend module
+	 *
+	 * @return void
+	 */
 	public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task) {
 		$task->deletefiles_directory = $submittedData['deletefiles_directory'];
 		$task->deletefiles_time = $submittedData['deletefiles_time'];
