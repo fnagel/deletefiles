f7dc003495eeaa8b7060a08c0b92c7fcee0ac0d2
Felix Nagel <info@felixnagel.com>
2014-08-23 16:40:32 +0200

    update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,12 +33,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'scheduler' => '',
-			'typo3' => '4.3.0-4.7.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"e2ec";s:16:"ext_autoload.php";s:4:"1100";s:12:"ext_icon.gif";s:4:"4a76";s:17:"ext_localconf.php";s:4:"ce0b";s:14:"doc/manual.sxw";s:4:"4cea";s:18:"lang/locallang.xml";s:4:"0c8c";s:19:"lang/locallang2.xml";s:4:"b1ed";s:38:"tasks/class.tx_deletefiles_cleaner.php";s:4:"d085";s:48:"tasks/class.tx_deletefiles_cleaner_addFields.php";s:4:"2fed";}',
 	'suggests' => array(),
 );
 
