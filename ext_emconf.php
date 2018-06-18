9e3608185bc1730fd8454220f174de3e70aad4a2
Felix Nagel <info@felixnagel.com>
2018-06-18 12:34:41 +0200

    [DOC] Add GDPR / DSGVO note in description

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Delete old files',
-    'description' => 'Scheduler task for deleting old files and folder. Supports FAL.',
+    'description' => 'Scheduler task for deleting old files and folder. Supports FAL. Useful for GDPR / DSGVO compliance.',
     'category' => 'services',
     'author' => 'Felix Nagel',
     'author_email' => 'info@felixnagel.com',
