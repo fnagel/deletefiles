fbc5197d71d0830eeac69d5e9157d8ec2d83d14c
Felix Nagel <info@felixnagel.com>
2019-10-29 23:09:15 +0100

    [TASK] Adopt license

--- Classes/Task/DeleteFilesTask.php
+++ Classes/Task/DeleteFilesTask.php
@@ -2,6 +2,13 @@
 
 namespace FelixNagel\DeleteFiles\Task;
 
+/**
+ * This file is part of the "deletefiles" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 
