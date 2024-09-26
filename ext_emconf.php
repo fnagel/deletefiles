5c97d55b850213294ce88c0a3b242801bc4eccc2
Felix Nagel <info@felixnagel.com>
2024-09-26 09:54:18 +0200

    [TASK] Allow PHP 8.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.3.1-dev',
     'constraints' => [
         'depends' => [
-            'php' => '8.0.0-8.2.99',
+            'php' => '8.0.0-8.3.99',
             'typo3' => '11.5.0-12.4.99',
             'scheduler' => '',
         ],
