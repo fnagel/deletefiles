eafd5cb2d84f0843823548f1b42aefcded3f356e
Felix Nagel <info@felixnagel.com>
2026-01-27 09:57:22 +0100

    [!!!][TASK] Support TYPO3 version 14.1 and remove support for v13.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,11 +17,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Felix Nagel',
     'author_email' => 'info@felixnagel.com',
     'state' => 'stable',
-    'version' => '1.5.1-dev',
+    'version' => '1.6.0-dev',
     'constraints' => [
         'depends' => [
             'php' => '8.2.0-8.4.99',
-            'typo3' => '13.0.0-13.4.99',
+            'typo3' => '14.0.0-14.1.99',
             'scheduler' => '',
         ],
         'conflicts' => [],
